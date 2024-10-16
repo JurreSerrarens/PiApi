37efa54fd1b8f5836ddd342e99a710f97cf37023
JurreSerrarens <jurre.serrarens@student.graafschapcollege.nl>
2024-10-16 10:20:07 +0200

    Remove fadhrenheit because cringe and error + L + ratio + no bitches + pls work now :(

--- main.py
+++ main.py
@@ -46,11 +46,11 @@ def read_temp():
 
 async def read_dht():
     temperature_c = sensor.temperature
-    temperature_f = temperature_c * (9 / 5) + 32
+    #temperature_f = temperature_c * (9 / 5) + 32
     humidity = sensor.humidity
-    print("Temp={0:0.1f}ºC, Temp={1:0.1f}ºF, Humidity={2:0.1f}%".format(temperature_c, temperature_f, humidity))
+    print("Temp={0:0.1f}ºC, Humidity={2:0.1f}%".format(temperature_c, humidity))
 
-    return temperature_c, temperature_f, humidity
+    return temperature_c,  humidity
 
 
 #API
