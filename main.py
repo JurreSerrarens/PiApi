1e126539a88aded2b32511ea43eac944881e5fa8
JurreSerrarens <jurre.serrarens@student.graafschapcollege.nl>
2024-11-04 14:17:29 +0100

    Bigger amount difference

--- main.py
+++ main.py
@@ -78,7 +78,7 @@ def clock():
                 print("WEEWOOWEEWOO")
                 motoron()
                 activated = True
-                print("Water amount: ",amount * ((read_temp()/40.0) * 2.0))
+                print("Water amount: ",amount * ((read_temp()/40.0) * 3.0))
                 time.sleep(amount * ((read_temp()/40.0) * 2.0))
                 
                 motoroff()
